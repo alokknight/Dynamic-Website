9e08b48cc7440bb491166d5b096c062c7f8b32fb
alokknight <alokpatel885@gmail.com>
2021-09-25 16:27:16 +0530

    make it better

--- coderpapa/urls.py
+++ coderpapa/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('', include('app1.urls')),
 
     ]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
